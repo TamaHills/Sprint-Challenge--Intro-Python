99ceb676513c270bb325945d0731a0892d2a478e
Matthew Masters <matt.d.m@hotmail.com>
2020-01-17 11:05:15 -0600

    ☑ completed city reader stretch and oop1

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,11 +1,12 @@
+import pathlib
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
 class City:
     def __init__(self, name, lat, lon):
         self.name = name
-        self.lat = lat
-        self.lon = lon
+        self.lat = float(lat)
+        self.lon = float(lon)
     def __repr__(self):
         return f'({self.name},{self.lat},{self.lon})'
 
@@ -27,7 +28,8 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    cities_txt = open('cities.csv', 'r').read()
+    directory = pathlib.Path(__file__).parent.absolute()
+    cities_txt = open(f'{directory}/cities.csv', 'r').read()
 
     cities_rows = cities_txt.split('\n')
     # remove column descriptors
@@ -82,8 +84,13 @@ for c in cities:
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
-  within = []
+  lat_upper = lat1 if lat1 > lat2 else lat2
+  lat_lower = lat1 if lat1 < lat2 else lat2
+  lon_upper = lon1 if lon1 > lon2 else lon2
+  lon_lower = lon1 if lon1 < lon2 else lon2
 
+  within_square = lambda lat, lon: lat >= lat_lower and lat <= lat_upper and lon >= lon_lower and lon <= lon_upper
+  within = [city for city in cities if within_square(city.lat, city.lon)]
   # TODO Ensure that the lat and lon valuse are all floats
   # Go through each city and check to see if it falls within 
   # the specified coordinates.
--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,26 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    pass #base class
+
+# Ground Vehicles
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+# Flight vehicle
+class FlightVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
